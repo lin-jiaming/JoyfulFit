bfe32c60bca10695aeb342e51257f10acf2a951d
lin-jiaming <1326700455@qq.com>
2018-06-07 17:18:40 +0800

    更新体重记录视图

--- JoyfulFit/Base/CourseViewController.swift
+++ JoyfulFit/Base/CourseViewController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+class CourseViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
+    //绑定collectionView
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate =  self
+        collectionView.dataSource = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+    //collectionView委托
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeightCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
